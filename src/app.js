e87be6c72374f588f118b8222219fc1fe939c01d
manju <manjuslv18@gmail.com>
2024-12-15 14:18:40 +0530

    Explore the Get post and Delete

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 
+
+app.get('/users',(req,res) => {
+    res.send({firstName: "manju",lastName: 'S'})
+})
+app.post('/users',(req,res) => {
+    res.send("data successfully saved to database")
+})
+app.delete('/users',(req,res) => {
+    res.send("data deleted from database")
+})
 app.use("/test",(req,res) => {
     res.send("this is test path");
 })
-app.use("/home",(req,res) => {
-    res.send("this is home");
-})
-app.use("/",(req,res) => {
-    res.send("this is normal path");
-})
+
 app.listen(4000,() => {
     console.log("server created on port 6000");
 })
